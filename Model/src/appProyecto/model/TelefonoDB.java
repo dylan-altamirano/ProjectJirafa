3af5f4a4375501a75fdcc32323b016172ee5dbd6
Dylan Altamirano <doaltamiranoce@est.utn.ac.cr>
2017-07-30 21:50:35 -0600

    cambios en telefonoDB

--- Model/src/appProyecto/model/TelefonoDB.java
+++ Model/src/appProyecto/model/TelefonoDB.java
@@ -62,4 +62,113 @@ public class TelefonoDB {
             
             }
         }
+    
+    public Telefono obtenerTelefono(String codigo, String idCliente) throws SNMPExceptions, SQLException {
+         //  ResultSet rsPA = null;
+           String strSQL = "";
+           Telefono telefono=null;
+           try {
+           //  open();
+               strSQL = 
+                       "Select ID, numero, tipo, estado from telefono where ID='"+codigo+"' and IDCliente='"+idCliente+"'";
+               //Se ejecuta la sentencia SQL
+               ResultSet rstelefono = accesoDatos.ejecutaSQLRetornaRS(strSQL);
+               while (rstelefono.next()) {
+                   
+                telefono = new Telefono();
+               
+                telefono.setID(codigo);
+                telefono.setNumero(rstelefono.getString("numero"));
+                telefono.setTipoTele(TipoTelefono.valueOf(rstelefono.getString("tipo"))); //Solo se trae el ID del tipo telefono para adjuntarle el objeto completo despues  
+                telefono.setEstado(rstelefono.getBoolean("estado"));   
+               
+               }
+               rstelefono.close();
+               
+           } catch (SQLException e) {
+               throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, 
+                                       e.getMessage(), e.getErrorCode());
+           }catch (Exception e) {
+               throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, 
+                                       e.getMessage());
+           } finally {
+            //  accesoDatos.closeResultSet(rsPA);
+          // close();
+           }
+           return telefono;
+       }
+    
+    public LinkedList<Telefono> obtenerTelefonosPorCliente(String idCliente) throws SNMPExceptions, SQLException {
+         //  ResultSet rsPA = null;
+           String strSQL = "";
+           Telefono telefono=null;
+           LinkedList<Telefono> listaTelefono = new LinkedList<Telefono>();
+           try {
+           //  open();
+               strSQL = 
+                       "Select ID, numero, tipo, estado from telefono where IDCliente="+idCliente;
+               //Se ejecuta la sentencia SQL
+               ResultSet rstelefono = accesoDatos.ejecutaSQLRetornaRS(strSQL);
+               while (rstelefono.next()) {
+                   
+                telefono = new Telefono();
+               
+                telefono.setID(rstelefono.getString("ID"));
+                telefono.setNumero(rstelefono.getString("numero"));
+                telefono.setTipoTele(TipoTelefono.valueOf(rstelefono.getString("tipo"))); //Solo se trae el ID del tipo telefono para adjuntarle el objeto completo despues  
+                telefono.setEstado(rstelefono.getBoolean("estado"));   
+                   
+                listaTelefono.add(telefono);   
+               
+               }
+               rstelefono.close();
+               
+           } catch (SQLException e) {
+               throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, 
+                                       e.getMessage(), e.getErrorCode());
+           }catch (Exception e) {
+               throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, 
+                                       e.getMessage());
+           } finally {
+            //  accesoDatos.closeResultSet(rsPA);
+          // close();
+           }
+           return listaTelefono;
+       }
+    
+    /**
+     *Consulta la existencia de un registro dentro del repositorio de datos.
+     * @param numero
+     * @return
+     * @throws SNMPExceptions
+     * @throws SQLException
+     */
+    public boolean consultarTelefono(int numero) throws SNMPExceptions, SQLException {
+           ResultSet rsTelefono = null;
+           String select = "";
+           try {
+            //  open();
+               boolean existe = false;
+               select = 
+               "Select ID, numero, tipo, estado where ID ="+numero;
+
+               
+               //Se ejecuta la sentencia SQL
+               rsTelefono = accesoDatos.ejecutaSQLRetornaRS(select);
+
+               if (rsTelefono.next()) {
+                   existe = true;
+               }
+               return existe;
+           } catch (SQLException e) {
+               throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, 
+                                       e.getMessage(), e.getErrorCode());
+           }catch (Exception e) {
+               throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, 
+                                       e.getMessage());
+           } finally {
+               accesoDatos.closeResultSet(rsTelefono);
+             // close(); 
+           }
+       }
 }
